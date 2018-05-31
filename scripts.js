7423c29ded9cee82c34b933ba75726ee6efea695
Guest User <Guest@Epicodus-10E.local>
2018-05-30 20:44:19 -0700

    add business logic, add ui elements for addition,subtraction,multiplication and division.

--- scripts.js
+++ scripts.js
@@ -1,8 +1,37 @@
-var add = function(number1,number2){
-return number1 + number2;
+
+// Business (or back-end) logic:
+
+var add = function(number1, number2) {
+  console.log(number1 + " in the add function.")
+  return number1 + number2;
 };
 
-var number1 = parseInt(prompt('Enter a number:'));
-var number2 = parseInt(prompt("Enter another number:"));
+var subtract = function(number1, number2) {
+  return number1 - number2;
+};
 
-alert(add(number1,number2));
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
+
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
+
+// UI:
+
+$(document).ready(function() {
+  console.log("ready");
+  //the problem is below this line
+  $("#add").submit(function(event) {
+    debugger;
+    console.log("submit happened.");
+    event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    console.log(number1);
+    var number2 = parseInt($("#add2").val());
+    var result = add(number1, number2);
+    $("#resultAdd").text(result);
+
+  });
+});
